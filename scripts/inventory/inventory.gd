119ed5611b683f8dca937cc6e5463bc8af62bbb1
luna-agente <gpt@rcscan.online>
2026-08-22 16:56:13 -0300

    feat: add new structural blocks to inventory

--- scripts/inventory/inventory.gd
+++ scripts/inventory/inventory.gd
@@ -8,15 +8,19 @@ const EMPTY := ""
 const METAL := "metal_1x1x1"
 const TRIANGLE := "triangle_1x1x1"
 const CYLINDER := "cylinder_1x1x1"
+const CUBE_1X2X1 := "cube_1x2x1"
+const HALF_CUBE := "half_cube_1x1x1"
+const WEDGE := "wedge_1x1x1"
+const CONE := "cone_1x1x1"
 
 var slots: Array[String] = [
 	METAL,
 	TRIANGLE,
 	CYLINDER,
-	EMPTY,
-	EMPTY,
-	EMPTY,
-	EMPTY,
+	CUBE_1X2X1,
+	HALF_CUBE,
+	WEDGE,
+	CONE,
 	EMPTY,
 	EMPTY,
 	EMPTY,
