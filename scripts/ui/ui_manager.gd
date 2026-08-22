b0ed990acacb4c5a126d4e0df494004fe0e07715
luna-agente <gpt@rcscan.online>
2026-08-22 17:20:27 -0300

    feat: add pause handling and fix mouse capture

--- scripts/ui/ui_manager.gd
+++ scripts/ui/ui_manager.gd
@@ -4,17 +4,19 @@ extends Node
 @export var crafting_menu_path: NodePath
 @export var hotbar_path: NodePath
 @export var player_path: NodePath
+@export var pause_menu_path: NodePath
 
 @onready var inventory_menu: Control = get_node(inventory_menu_path) as Control
 @onready var crafting_menu: Control = get_node(crafting_menu_path) as Control
 @onready var hotbar: Control = get_node(hotbar_path) as Control
 @onready var player: Node3D = get_node(player_path) as Node3D
+@onready var pause_menu: Control = get_node(pause_menu_path) as Control
 
 func _ready() -> void:
 	_set_mouse_captured()
 
 func _process(_delta: float) -> void:
-	var menu_open: bool = inventory_menu.visible or crafting_menu.visible
+	var menu_open: bool = inventory_menu.visible or crafting_menu.visible or pause_menu.visible
 	player.set_process(not menu_open)
 	player.set_process_input(not menu_open)
 	hotbar.visible = not menu_open
@@ -29,21 +31,34 @@ func _unhandled_key_input(event: InputEvent) -> void:
 
 	match event.keycode:
 		KEY_I:
-			if crafting_menu.visible:
-				crafting_menu.toggle()
-			inventory_menu.toggle()
+			if not pause_menu.visible:
+				if crafting_menu.visible:
+					crafting_menu.toggle()
+				inventory_menu.toggle()
 			get_viewport().set_input_as_handled()
 		KEY_T:
-			if inventory_menu.visible:
-				inventory_menu.toggle()
-			crafting_menu.toggle()
+			if not pause_menu.visible:
+				if inventory_menu.visible:
+					inventory_menu.toggle()
+				crafting_menu.toggle()
 			get_viewport().set_input_as_handled()
 		KEY_ESCAPE:
 			if inventory_menu.visible:
 				inventory_menu.toggle()
 			elif crafting_menu.visible:
 				crafting_menu.toggle()
+			else:
+				toggle_pause()
 			get_viewport().set_input_as_handled()
+
+func toggle_pause() -> void:
+	pause_menu.toggle()
+
+func resume_game() -> void:
+	pause_menu.visible = false
+	inventory_menu.visible = false
+	crafting_menu.visible = false
+	_set_mouse_captured()
 
 func _set_mouse_captured() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
