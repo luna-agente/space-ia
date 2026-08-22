0b7436a0bbb4fd6811e704385eedf8c7fb2c4778
luna-agente <gpt@rcscan.online>
2026-08-22 17:20:47 -0300

    fix: block gameplay and mouse capture from main menu

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,20 +1,24 @@
 extends Control
 
-@export var gameplay_root_path: NodePath
-@export var menu_root_path: NodePath
+@export var player_path: NodePath
+@export var build_system_path: NodePath
+@export var ui_manager_path: NodePath
 
-@onready var gameplay_root: Node = get_node(gameplay_root_path)
-@onready var menu_root: Control = get_node(menu_root_path)
+@onready var player: Node = get_node(player_path)
+@onready var build_system: Node = get_node(build_system_path)
+@onready var ui_manager: Node = get_node(ui_manager_path)
 
 func _ready() -> void:
 	visible = true
-	gameplay_root.process_mode = Node.PROCESS_MODE_DISABLED
+	player.process_mode = Node.PROCESS_MODE_DISABLED
+	build_system.process_mode = Node.PROCESS_MODE_DISABLED
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _on_play_pressed() -> void:
 	visible = false
-	gameplay_root.process_mode = Node.PROCESS_MODE_INHERIT
-	menu_root.grab_focus()
+	player.process_mode = Node.PROCESS_MODE_INHERIT
+	build_system.process_mode = Node.PROCESS_MODE_INHERIT
+	ui_manager.call("resume_game")
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _on_quit_pressed() -> void:
